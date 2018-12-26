8467d19e929c4358d5408290092a3a55dcd2c3fa
Masa <masa@simasasolutions.com>
2018-12-26 15:30:46 +0200

    Moved group getter to Chat

--- src/main/java/fi/matiaspaavilainen/masuitecore/MaSuiteCore.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/MaSuiteCore.java
@@ -1,12 +1,9 @@
 package fi.matiaspaavilainen.masuitecore;
-import net.milkbowl.vault.chat.Chat;
+
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MaSuiteCore extends JavaPlugin implements Listener {
-
-    private static Chat chat = null;
 
     @Override
     public void onEnable() {
@@ -14,24 +11,7 @@ public class MaSuiteCore extends JavaPlugin implements Listener {
         getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new PluginMessageListener(this));
         getServer().getPluginManager().registerEvents(this, this);
-        setupChat();
 
-        if(chat == null){
-            getServer().getPluginManager().disablePlugin(this);
-            System.out.println("[MaSuite] [Core] Vault not found... Disabling...");
-        }
     }
 
-    private boolean setupChat() {
-        RegisteredServiceProvider<Chat> chatProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.chat.Chat.class);
-        if (chatProvider != null) {
-            chat = chatProvider.getProvider();
-        }
-
-        return (chat != null);
-    }
-
-    public Chat getChat() {
-        return chat;
-    }
 }
--- src/main/java/fi/matiaspaavilainen/masuitecore/PluginMessageListener.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/PluginMessageListener.java
@@ -43,44 +43,11 @@ public class PluginMessageListener implements org.bukkit.plugin.messaging.Plugin
                 out.writeFloat(loc.getPitch());
                 player.sendPluginMessage(plugin, "BungeeCord", b.toByteArray());
             }
-            if (subchannel.equals("MaSuitePlayerGroup")) {
-                Player p = Bukkit.getPlayer(UUID.fromString(in.readUTF()));
-                if (p == null) {
-                    return;
-                }
-                out.writeUTF("MaSuitePlayerGroup");
-                out.writeUTF(String.valueOf(p.getUniqueId()));
-                out.writeUTF(getPrefix(p));
-                out.writeUTF(getSuffix(p));
-                player.sendPluginMessage(plugin, "BungeeCord", b.toByteArray());
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    private String getPrefix(Player p) {
-        if (plugin.getChat() != null) {
-            if (plugin.getChat().getPlayerPrefix(p) != null) {
-                return plugin.getChat().getPlayerPrefix(p);
-            } else if (plugin.getChat().getGroupPrefix(p.getWorld(), plugin.getChat().getPrimaryGroup(p)) != null) {
-                return plugin.getChat().getGroupPrefix(p.getWorld(), plugin.getChat().getPrimaryGroup(p));
-            }
-            return "";
-        }
-        return "";
-    }
 
-    private String getSuffix(Player p) {
-        if (plugin.getChat() != null) {
-            if (plugin.getChat().getPlayerSuffix(p) != null) {
-                return plugin.getChat().getPlayerSuffix(p);
-            } else if (plugin.getChat().getGroupSuffix(p.getWorld(), plugin.getChat().getPrimaryGroup(p)) != null) {
-                return plugin.getChat().getGroupSuffix(p.getWorld(), plugin.getChat().getPrimaryGroup(p));
-            }
-            return "";
-        }
-        return "";
-    }
 }
